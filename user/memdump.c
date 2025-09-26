ed571bce92499f2f1285a37a26213237d9bcbfa0
root <root@SaadHP.localdomain>
2025-09-26 19:46:55 +0500

    PS#1 (util): implement memdump (i,p,h,c,s,S)

--- user/memdump.c
+++ user/memdump.c
@@ -60,6 +60,78 @@ main(int argc, char *argv[])
 void
 memdump(char *fmt, char *data)
 {
-  // Your code here.
+  // Walk 'data' as raw bytes
+  unsigned char *p = (unsigned char *)data;
 
+  for (char *f = fmt; *f; f++) {
+    switch (*f) {
+
+      case 'i': {                      // next 4 bytes: 32-bit int (decimal)
+        unsigned int v;
+        memmove(&v, p, 4);
+        p += 4;
+        printf("%d\n", (int)v);
+        break;
+      }
+
+      case 'p': {                      // next 8 bytes: 64-bit int (hex, no 0x)
+        unsigned long long v;
+        memmove(&v, p, 8);
+        p += 8;
+
+        // print v in lowercase hex without leading zeros
+        if (v == 0) {
+          printf("0\n");
+        } else {
+          char buf[17];                // 16 hex nibbles + NUL
+          int i = 16;
+          buf[i] = 0;
+          while (v && i > 0) {
+            int nib = v & 0xF;
+            buf[--i] = (nib < 10) ? ('0' + nib) : ('a' + nib - 10);
+            v >>= 4;
+          }
+          printf("%s\n", &buf[i]);
+        }
+        break;
+      }
+
+      case 'h': {                      // next 2 bytes: 16-bit int (decimal)
+        unsigned short v;
+        memmove(&v, p, 2);
+        p += 2;
+        printf("%d\n", (int)v);
+        break;
+      }
+
+      case 'c': {                      // next 1 byte: ASCII char
+        unsigned char v = *p++;
+        printf("%c\n", (char)v);
+        break;
+      }
+
+      case 's': {                      // next 8 bytes: pointer to C-string
+        unsigned long long addr;
+        memmove(&addr, p, 8);
+        p += 8;
+        char *sp = (char *)addr;
+        printf("%s\n", sp);
+        break;
+      }
+
+      case 'S': {                      // rest of data is a C-string
+        char *sp = (char *)p;
+        printf("%s\n", sp);
+        while (*p) p++;                // consume bytes up to NUL
+        p++;                           // skip the NUL
+        break;
+      }
+
+      default:
+        // ignore unknown format chars
+        break;
+    }
+  }
 }
+
+
